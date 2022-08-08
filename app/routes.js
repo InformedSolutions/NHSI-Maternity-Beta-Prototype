73c57660978277b4a801f7c8187d1784e68d3f60
Bristow <bristowh@informed.co.uk>
2022-08-08 14:51:16 +0100

    updates for sprint 8

--- app/routes.js
+++ app/routes.js
@@ -6,3 +6,17 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// event type routing
+
+router.post('/event-type', function (req, res) {
+    var event = req.body['event-type'];
+
+    if (event == 'brain-injury') {
+            res.redirect('/brain-injury/task-list')
+
+    } else {
+            res.redirect('/task-list')
+
+    }
+});
